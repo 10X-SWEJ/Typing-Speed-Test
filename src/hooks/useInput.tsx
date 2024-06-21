3a06703eab65d87700381699b7ee419f978eb5d3
tenxfounder <tenxfounder@gmail.com>
2024-06-21 12:54:50 +0000

    fixed: key

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from "react";
 
 export const useInput = function () {
-   const [typedWord, setTypedWord] = useState<string[]>([]);
+  const [typedWord, setTypedWord] = useState<string[]>([]);
 
-   function handleKeydown(e: KeyboardEvent) {
-      // /^[A-Za-z\s,.'"]$/
-      if (e.key.match(/^[a-zA-Z0-9"'\-,:'.]+$/))
-         setTypedWord((typedWord) => [...typedWord, e.key]);
-   }
+  function handleKeydown(e: KeyboardEvent) {
+    // /^[A-Za-z\s,.'"]$/
+    if (
+      (e.key !== "Shift" && e.key.match(/^[a-zA-Z0-9"'\-,:'."]+$/)) ||
+      e.key === " "
+    )
+      setTypedWord((typedWord) => [...typedWord, e.key]);
+  }
 
-   useEffect(() => {
-      window.addEventListener("keydown", handleKeydown);
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
 
-      return () => window.removeEventListener("keydown", handleKeydown);
-   }, [handleKeydown]);
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [handleKeydown]);
 
-   return typedWord;
+  return typedWord;
 };
--- src/hooks/useTimer.tsx
+++ src/hooks/useTimer.tsx
@@ -1,24 +1,24 @@
 import { useRef, useEffect, useState } from "react";
 
 export const useTimer = function () {
-   const [startCountdown, setStartCountdown] = useState(false);
-   const [second, setSecond] = useState(0);
-   const intervalId = useRef(null); // How to set type here
+  const [startCountdown, setStartCountdown] = useState(false);
+  const [second, setSecond] = useState(0);
+  const intervalId = useRef<null | number>(null); 
 
-   useEffect(() => {
-      if (startCountdown) {
-         intervalId.current = setInterval(() => {
-            setSecond((second) => second + 1);
-         }, 1000);
-      } else {
-         clearInterval(intervalId.current!);
-         intervalId.current = null;
-      }
+  useEffect(() => {
+    if (startCountdown) {
+      intervalId.current = setInterval(() => {
+        setSecond((second) => second + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId.current!);
+      intervalId.current = null;
+    }
 
-      return () => {
-         clearInterval(intervalId.current!);
-      };
-   }, [startCountdown]);
+    return () => {
+      clearInterval(intervalId.current!);
+    };
+  }, [startCountdown]);
 
-   return { second, setStartCountdown };
+  return { second, setStartCountdown };
 };
